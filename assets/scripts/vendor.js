dadc243013d6612328e6aaf4a3e4edc5e26e004a
Cristina Kelly <cristinakellyt@gmail.com>
2022-09-08 14:17:29 +0200

    Change btn set to altern between annual and monthly plan on the card

--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -8,10 +8,15 @@ const btnMonthly = document.getElementById('monthly');
 const planValue = document.getElementById('plan-value');
 const totalValue = document.getElementById('total-value');
 const fee = document.getElementById('fee');
+const btnChange = document.getElementById('btn-change');
+const planType = document.getElementById('plan-type');
+const planPrice = document.getElementById('plan-price');
 const dollarSign = '$';
 const annualPrice = 59.99;
 const monthlyPrice = 66.65;
 const feeValue = 1.0;
+
+setCardPlanAndPrice('Annual Plan', dollarSign, annualPrice, '/year');
 
 function setTotalValue(dollar, billingCycle, feePrice) {
   totalValue.textContent =
@@ -22,45 +27,55 @@ function setPlanValue(dollar, billingCycle) {
   planValue.textContent = dollar + billingCycle;
 }
 
-function showPopUp() {
+function showPopUpHandler() {
   popUp.classList.remove('slideout-animation');
   cta.classList.add('slidein-animation');
   popUp.style.display = 'flex';
 
-  setPlanValue(dollarSign, annualPrice);
   fee.textContent = dollarSign + parseFloat(feeValue).toPrecision(3);
+  setPlanValue(dollarSign, annualPrice);
   setTotalValue(dollarSign, annualPrice, feeValue);
 }
 
-function closePopUp() {
+function closePopUpHandler() {
   cta.classList.remove('slidein-animation');
   popUp.classList.add('slideout-animation');
 
   setTimeout(delayClosePopUp, 320);
 }
 
-function delayClosePopUp() {
-  popUp.style.display = 'none';
-}
-
-function showAnnualPrice() {
+function showAnnualPriceHandler() {
   setPlanValue(dollarSign, annualPrice);
   setTotalValue(dollarSign, annualPrice, feeValue);
 }
 
-function showMonthlyPrice() {
+function showMonthlyPriceHandler() {
   setPlanValue(dollarSign, monthlyPrice);
   setTotalValue(dollarSign, monthlyPrice, feeValue);
 }
 
-btnPayment.addEventListener('click', showPopUp);
-btnBack.addEventListener('click', closePopUp);
-btnAnnual.addEventListener('click', showAnnualPrice);
-btnMonthly.addEventListener('click', showMonthlyPrice);
+function changePlanHandler() {
+  if (planType.textContent == 'Annual Plan') {
+    setCardPlanAndPrice('Monthly Plan', dollarSign, monthlyPrice, '/month');
+  } else {
+    setCardPlanAndPrice('Annual Plan', dollarSign, annualPrice, '/year');
+  }
+}
 
-// setTotalValue() settar o value do totoal value - OK
-// setPlanValue() settart o value to Plan price -OK
-// Inticializacao deve ser feita chamando as funcoes criadas acima.
-// O botao de change do carde initiacal quando clicado vai ficar alternando entre o valor de Anual and montly.
+function setCardPlanAndPrice(plan, currency, costPlan, time) {
+  planType.textContent = plan;
+  planPrice.textContent = currency + costPlan + time;
+}
+
+function delayClosePopUp() {
+  popUp.style.display = 'none';
+}
+
+btnPayment.addEventListener('click', showPopUpHandler);
+btnBack.addEventListener('click', closePopUpHandler);
+btnAnnual.addEventListener('click', showAnnualPriceHandler);
+btnMonthly.addEventListener('click', showMonthlyPriceHandler);
+btnChange.addEventListener('click', changePlanHandler);
+
 // O plano escolhido na tela principal, deve ser o defaullt da tela proceeed to payment.
 //lembre-se que vc tem o poder de fazer o que quiser ons metodso de abrir e fechar o pop-up
